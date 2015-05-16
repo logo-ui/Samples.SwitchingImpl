03c84c1b5944c1b20f490cd03932cfeace506faa
godrose <godrose@gmail.com>
2015-05-16 13:10:11 +0300

    The model dependencies registration is supplied via module with no hard reference in the Shell project

---
+++ LogoUI.Samples.Client.Gui.Shell/AppBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using LogoFX.UI.Bootstrapping.SimpleContainer;
+using LogoFX.UI.Navigation;
+using LogoUI.Samples.Client.Gui.Shell.UiServices;
+using LogoUI.Samples.Client.Gui.Shell.ViewModels;
+
+namespace LogoUI.Samples.Client.Gui.Shell
+{
+	public sealed class AppBootstrapper : NavigationBootstrapper<ShellViewModel,ExtendedSimpleIocContainer>
+	{
+	    protected override void OnConfigure(ExtendedSimpleIocContainer container)
+		{
+            base.OnConfigure(container);            
+            
+            container.RegisterSingleton<IShellCloseService, ShellViewModel>();
+            container.RegisterSingleton<IWindowManager, ShellViewModel>("LikeRT");             
+        }	    
+	}
+}
---
+++ LogoUI.Samples.Client.Gui.Shell/ViewModels/MainViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using LogoFX.Practices.IoC;
+using LogoFX.UI.Navigation;
+using LogoUI.Samples.Client.Gui.Shell.Compliance.ViewModels;
+
+namespace LogoUI.Samples.Client.Gui.Shell.ViewModels
+{    
+    [Singleton]
+    [NavigationViewModel(ConductorType = typeof (ShellViewModel), IsSingleton = true)]
+    [NavigationSynonym(typeof(IMainViewModel))]
+    public sealed class MainViewModel : Conductor<IScreen>, INavigationViewModel, IMainViewModel
+    {
+        public ComplianceRootViewModel ComplianceRootViewModel { get; set; }
+        private readonly INavigationService _navigationService;
+
+        public MainViewModel(
+            INavigationService navigationService,
+            ComplianceRootViewModel complianceRootViewModel)
+        {
+            ComplianceRootViewModel = complianceRootViewModel;
+            _navigationService = navigationService;
+        }
+
+
+        protected override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            NavigationHelper.RegisterNavigationCommands(typeof(MainViewModel),view, _navigationService);
+        }                
+
+        void INavigationConductor.NavigateTo(object viewModel, object argument)
+        {
+            ActivateItemImpl(viewModel);            
+        }                
+
+        private void ActivateItemImpl(object viewModel)
+        {
+            ActivateItem((IScreen)viewModel);
+        }
+
+        public void OnNavigated(NavigationDirection direction, object argument)
+        {
+            ActivateItemImpl(ComplianceRootViewModel);
+        }
+    }
+}
